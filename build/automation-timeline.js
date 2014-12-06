a89c3240635e3737a87a5dc27927ef116a7030b7
Jordan Santell <jsantell@gmail.com>
2014-12-05 18:49:26 -0800

    Remove debugging, v1.0.1

--- build/automation-timeline.js
+++ build/automation-timeline.js
@@ -1,7 +1,7 @@
 /**
- * web-audio-automation-timeline - 1.0.0
+ * web-audio-automation-timeline - 1.0.1
  * https://github.com/jsantell/web-audio-automation-timeline
- * Copyright (c) 2014 Jordan Santell
+ * MIT License, copyright (c) 2014 Jordan Santell
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Timeline=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = require("./lib/timeline").Timeline;
@@ -55,7 +55,6 @@ exports.extractValueFromCurve = function (start, curve, curveLength, duration, t
     return curve[curveLength - 1];
   }
 
-  console.log("time: ", t, "index: ", ~~(curveLength*ratio), "value: ", curve[~~(curveLength*ratio)]);
   return curve[~~(curveLength * ratio)];
 };
 
